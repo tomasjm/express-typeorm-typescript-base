92403a11b8edb0640e9f3500b32970708f820e51
Tomás Jiménez <tjimenez999@gmail.com>
2019-06-24 00:26:54 -0400

    asd

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ let config = {
       clientSecret: "secret"
     },
     facebook: {
-      clientID: "654446205027302",
-      clientSecret: "397027a39d3a8618a329a79f4b61d8bc"
+      clientID: "302",
+      clientSecret: "bc"
     }
   }
 };
